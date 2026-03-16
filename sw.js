3d7248d976e66e1fc9397db3693876865ad7f855
misojun0-design <misojun0@hanmail.net>
2026-03-16 19:49:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,23 @@
-var CACHE = 'pf-v27';
-self.addEventListener('install', function(e) {
+var CACHE='pf-v28';
+self.addEventListener('install',function(e){
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) {
-      return c.addAll(['./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(function(){});
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(function(c){
+    return c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(function(){});
+  }));
 });
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate',function(e){
   self.clients.claim();
-  e.waitUntil(caches.keys().then(function(ks) {
+  e.waitUntil(caches.keys().then(function(ks){
     return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
   }));
 });
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch',function(e){
   if(e.request.method!=='GET') return;
   e.respondWith(
-    fetch(e.request).then(function(r) {
-      if(r&&r.status===200){
-        var clone=r.clone();
-        caches.open(CACHE).then(function(c){c.put(e.request,clone);});
-      }
+    fetch(e.request).then(function(r){
+      if(r&&r.status===200){var clone=r.clone();caches.open(CACHE).then(function(c){c.put(e.request,clone);});}
       return r;
-    }).catch(function() {
+    }).catch(function(){
       return caches.match(e.request).then(function(c){return c||caches.match('./index.html');});
     })
   );
